002028a6e1e6461cd82a594ffb8ed874258de677
Werner Keil <werner.keil@gmx.net>
2013-01-05 02:34:21 +0100

    Update jdk-stub/src/main/java/net/java/javamoney/ri/DefaultCurrencyUnitDataProvider.java
    Updated license header

--- jdk-stub/src/main/java/net/java/javamoney/ri/DefaultCurrencyUnitDataProvider.java
+++ jdk-stub/src/main/java/net/java/javamoney/ri/DefaultCurrencyUnitDataProvider.java
@@ -1,34 +1,25 @@
+/*
+ *  Copyright (c) 2012, 2013, Werner Keil, Creddit Suisse (Anatole Tresch).
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ * Contributors:
+ *    Werner Keil - initial API and implementation
+ *    Anatole Tresch - refactored and use of SPI
+ */
 package net.java.javamoney.ri;
 
-<<<<<<< HEAD:jdk-stub/src/main/java/javamoney/util/money/DefaultCurrencyUnitDataProvider.java
 import javax.money.spi.*;
-
-/*
- * Copyright (c) 2000, 2012, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
-=======
->>>>>>> a63812e1178535e10625662ed6299a6669e1d3fa:jdk-stub/src/main/java/net/java/javamoney/ri/DefaultCurrencyUnitDataProvider.java
 
 /**
  * Provider for available currencies using a file.

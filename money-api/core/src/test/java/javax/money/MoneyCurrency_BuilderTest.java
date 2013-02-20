e9d67613537eaecb474d67a7264ce6c05e73b480
Anatole Tresch <atsticks@gmail.com>
2013-02-20 16:53:04 +0100

    Removed unused imports.

--- money-api/core/src/test/java/javax/money/MoneyCurrency_BuilderTest.java
+++ money-api/core/src/test/java/javax/money/MoneyCurrency_BuilderTest.java
@@ -36,7 +36,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.util.Enumeration;
 

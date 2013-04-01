e3a4ce43b170363c751f2ed5347d77675bd69dff
Anatole Tresch <atsticks@gmail.com>
2013-04-02 00:24:37 +0200

    Updated exception class.

--- money-api/convert/src/main/java/javax/money/convert/CurrencyConversionException.java
+++ money-api/convert/src/main/java/javax/money/convert/CurrencyConversionException.java
@@ -16,6 +16,7 @@ import javax.money.MonetaryException;
  * 
  * @author Werner Keil
  * @author Stephen Colebourne
+ * @author Anatole Tresch
  */
 public class CurrencyConversionException extends MonetaryException {
 

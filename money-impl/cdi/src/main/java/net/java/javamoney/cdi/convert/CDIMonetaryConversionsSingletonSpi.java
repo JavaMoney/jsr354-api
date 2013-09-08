fc57faee35f0c03f0a9286425e18ab89ad94db68
Anatole Tresch <atsticks@gmail.com>
2013-09-08 10:40:32 +0200

    - Added CompoundConversionProvider. - Used it for fixing implementation in se-cdi.

--- money-impl/cdi/src/main/java/net/java/javamoney/cdi/convert/CDIMonetaryConversionsSingletonSpi.java
+++ money-impl/cdi/src/main/java/net/java/javamoney/cdi/convert/CDIMonetaryConversionsSingletonSpi.java
@@ -1,20 +1,19 @@
 /*
- *  Copyright (c) 2012, 2013, Credit Suisse (Anatole Tresch), Werner Keil.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- * Contributors:
- *    Anatole Tresch - initial implementation.
+ * Copyright (c) 2012, 2013, Credit Suisse (Anatole Tresch), Werner Keil.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * Contributors: Anatole Tresch - initial implementation.
  */
 package net.java.javamoney.cdi.convert;
 
@@ -26,39 +25,52 @@ import java.util.concurrent.ConcurrentHashMap;
 import javax.money.convert.ConversionProvider;
 import javax.money.convert.ExchangeRateType;
 import javax.money.convert.spi.MonetaryConversionsSingletonSpi;
+
 import net.java.javamoney.cdi.CDIContainer;
+import net.java.javamoney.ri.convert.provider.CompoundConversionProvider;
 
-public class CDIMonetaryConversionsSingletonSpi implements MonetaryConversionsSingletonSpi {
+public class CDIMonetaryConversionsSingletonSpi implements
+		MonetaryConversionsSingletonSpi {
 
-    private Map<ExchangeRateType, ConversionProvider> conversionProviders = new ConcurrentHashMap<ExchangeRateType, ConversionProvider>();
+	private Map<ExchangeRateType, CompoundConversionProvider> conversionProviders = new ConcurrentHashMap<ExchangeRateType, CompoundConversionProvider>();
 
-    public CDIMonetaryConversionsSingletonSpi() {
-        reload();
-    }
+	public CDIMonetaryConversionsSingletonSpi() {
+		reload();
+	}
 
-    public void reload() {
-        for (ConversionProvider prov : CDIContainer.getInstances(ConversionProvider.class)) {
-            this.conversionProviders.put(prov.getExchangeRateType(), prov);
-        }
-    }
+	public void reload() {
+		for (ConversionProvider prov : CDIContainer
+				.getInstances(ConversionProvider.class)) {
+			CompoundConversionProvider provider = this.conversionProviders
+					.get(prov.getExchangeRateType());
+			if (provider == null) {
+				provider = new CompoundConversionProvider(
+						prov.getExchangeRateType());
+				this.conversionProviders.put(prov.getExchangeRateType(),
+						provider);
 
-    @Override
-    public ConversionProvider getConversionProvider(ExchangeRateType type) {
-        ConversionProvider prov = this.conversionProviders.get(type);
-        if (prov == null) {
-            throw new IllegalArgumentException("Unsupported ExchangeRateType: "
-                    + type);
-        }
-        return prov;
-    }
+			}
+			provider.addProvider(prov);
+		}
+	}
 
-    @Override
-    public Collection<ExchangeRateType> getSupportedExchangeRateTypes() {
-        return Collections.unmodifiableCollection(conversionProviders.keySet());
-    }
+	@Override
+	public ConversionProvider getConversionProvider(ExchangeRateType type) {
+		CompoundConversionProvider prov = this.conversionProviders.get(type);
+		if (prov == null) {
+			throw new IllegalArgumentException("Unsupported ExchangeRateType: "
+					+ type);
+		}
+		return prov;
+	}
 
-    @Override
-    public boolean isSupportedExchangeRateType(ExchangeRateType type) {
-        return conversionProviders.containsKey(type);
-    }
+	@Override
+	public Collection<ExchangeRateType> getSupportedExchangeRateTypes() {
+		return Collections.unmodifiableCollection(conversionProviders.keySet());
+	}
+
+	@Override
+	public boolean isSupportedExchangeRateType(ExchangeRateType type) {
+		return conversionProviders.containsKey(type);
+	}
 }

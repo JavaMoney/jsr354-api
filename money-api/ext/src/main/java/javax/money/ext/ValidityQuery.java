a47542aa218d502aded1fd3157a2f3cc926f65cc
keilw <werner.keil@gmx.net>
2013-09-23 11:45:49 -0700

    Updated build scripts and POMs

--- money-api/ext/src/main/java/javax/money/ext/ValidityQuery.java
+++ money-api/ext/src/main/java/javax/money/ext/ValidityQuery.java
@@ -139,7 +139,7 @@ public class ValidityQuery<T> {
 	/**
 	 * Get the starting range, for which validities are queried.0
 	 * 
-	 * @return the from timestamp, or {@code null].
+	 * @return the from timestamp, or {@code null].
 	 */
 	public final Long getFrom() {
 		return from;
@@ -148,7 +148,7 @@ public class ValidityQuery<T> {
 	/**
 	 * Get the ending range, for which validities are queried.0
 	 * 
-	 * @return the to timestamp, or {@code null].
+	 * @return the to timestamp, or {@code null].
 	 */
 	public final Long getTo() {
 		return to;

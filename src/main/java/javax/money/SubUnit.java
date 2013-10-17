2761c5e7a4782a40e6f8e2b9df86de74f2c21f59
keilw <werner.keil@gmx.net>
2013-10-17 02:42:54 +0200

    Updated JavaDoc

--- src/main/java/javax/money/SubUnit.java
+++ src/main/java/javax/money/SubUnit.java
@@ -15,6 +15,7 @@ package javax.money;
  * <p>
  * In a currency, there is usually a main unit (base), and a subunit that is a fraction of the main unit.
  * In some countries, there are multiple levels of subunits.
+ * A few countries (at least <b>Iran</b> is documented) use subunits based on multiples of the main unit.
  * <p>
  * Currency subunits can be distinguished by separate {@link #getId()} Ids. Each Id is unique <b>per currency</b>.
  * <h4>Implementation specification</h4>
@@ -28,7 +29,7 @@ package javax.money;
  * </ul>
  * 
  * @author Werner Keil
- * @version 0.2
+ * @version 0.3
  * 
  * @see <a href="http://en.wikipedia.org/wiki/Denomination_(currency)">Wikipedia: Denomination (currency)</a>
  * @see <a href="http://publications.europa.eu/code/en/en-5000500.htm#fn-4">EU: Currency subunit</a>

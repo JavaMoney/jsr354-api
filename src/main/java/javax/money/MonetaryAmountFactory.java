75eff77e65683bebcf8b843de77eb12748750f00
Anatole Tresch <atsticks@gmail.com>
2014-06-04 08:11:30 +0200

    Fixed useless check.

--- src/main/java/javax/money/MonetaryAmountFactory.java
+++ src/main/java/javax/money/MonetaryAmountFactory.java
@@ -33,8 +33,9 @@ package javax.money;
  * </ul>
  * If not defined a default {@link MonetaryContext} is used, which can also be configured by adding
  * configuration to a file {@code /javamoney.properties} to the classpath.
- * 
- * Hereby the entries. e.g. for a class {@code MyMoney} should start with {@code a.b.MyMoney.ctx}. The entries valid must be documented
+ * <p>
+ * Hereby the entries. e.g. for a class {@code MyMoney} should start with {@code a.b.MyMoney.ctx}. The entries valid
+ * must be documented
  * on the according implementation class, where the following entries are defined for all implementation types
  * (example below given for a class {@code a.b.MyMoney}:
  * <ul>
@@ -45,147 +46,140 @@ package javax.money;
  * <p>
  * <h2>Implementation specification</h2> Instances of this interface are <b>not</b> required to be
  * thread-safe!
- * 
- * @version 0.6.1
+ *
  * @author Anatole Tresch
  * @author Werner Keil
+ * @version 0.6.1
  */
-public interface MonetaryAmountFactory<T extends MonetaryAmount> {
+public interface MonetaryAmountFactory<T extends MonetaryAmount>{
 
-	/**
-	 * Access the {@link MonetaryAmount} implementation type.
-	 * 
-	 * @return the {@link MonetaryAmount} implementation type, never {@code null}.
-	 */
-	Class<? extends MonetaryAmount> getAmountType();
+    /**
+     * Access the {@link MonetaryAmount} implementation type.
+     *
+     * @return the {@link MonetaryAmount} implementation type, never {@code null}.
+     */
+    Class<? extends MonetaryAmount> getAmountType();
 
-	/**
-	 * Sets the {@link CurrencyUnit} to be used.
-	 * 
-	 * @param currencyCode
-	 *            the currencyCode of the currency to be used, not {@code null}. The currency code
-	 *            will be resolved using {@link MonetaryCurrencies#getCurrency(String)}.
-	 * @throws UnknownCurrencyException
-	 *             if the {@code currencyCode} is not resolvable.
-	 * @return This factory instance, for chaining.
-	 */
-	default MonetaryAmountFactory<T> setCurrency(String currencyCode){
+    /**
+     * Sets the {@link CurrencyUnit} to be used.
+     *
+     * @param currencyCode the currencyCode of the currency to be used, not {@code null}. The currency code
+     *                     will be resolved using {@link MonetaryCurrencies#getCurrency(String)}.
+     * @return This factory instance, for chaining.
+     * @throws UnknownCurrencyException if the {@code currencyCode} is not resolvable.
+     */
+    default MonetaryAmountFactory<T> setCurrency(String currencyCode){
         return setCurrency(MonetaryCurrencies.getCurrency(currencyCode));
     }
 
-	/**
-	 * Sets the {@link CurrencyUnit} to be used.
-	 * 
-	 * @param currency
-	 *            the {@link CurrencyUnit} to be used, not {@code null}
-	 * @return This factory instance, for chaining.
-	 */
-	MonetaryAmountFactory<T> setCurrency(CurrencyUnit currency);
+    /**
+     * Sets the {@link CurrencyUnit} to be used.
+     *
+     * @param currency the {@link CurrencyUnit} to be used, not {@code null}
+     * @return This factory instance, for chaining.
+     */
+    MonetaryAmountFactory<T> setCurrency(CurrencyUnit currency);
 
-	/**
-	 * Sets the number to be used.
-	 * 
-	 * @param number
-	 *            the number to be used
-	 * @return This factory instance, for chaining.
-	 */
-	MonetaryAmountFactory<T> setNumber(double number);
+    /**
+     * Sets the number to be used.
+     *
+     * @param number the number to be used
+     * @return This factory instance, for chaining.
+     */
+    MonetaryAmountFactory<T> setNumber(double number);
 
-	/**
-	 * Sets the number to be used.
-	 * 
-	 * @param number
-	 *            the number to be used
-	 * @return This factory instance, for chaining.
-	 */
-	MonetaryAmountFactory<T> setNumber(long number);
+    /**
+     * Sets the number to be used.
+     *
+     * @param number the number to be used
+     * @return This factory instance, for chaining.
+     */
+    MonetaryAmountFactory<T> setNumber(long number);
 
-	/**
-	 * Sets the number to be used.
-	 * 
-	 * @param number
-	 *            the number to be used, not {@code null}.
-	 * @return This factory instance, for chaining.
-	 */
-	MonetaryAmountFactory<T> setNumber(Number number);
+    /**
+     * Sets the number to be used.
+     *
+     * @param number the number to be used, not {@code null}.
+     * @return This factory instance, for chaining.
+     */
+    MonetaryAmountFactory<T> setNumber(Number number);
 
     /**
      * Get the maximum possible number that this type can represent. If the numeric model has no limitations on the
-     * numeric range, null should be returned.
+     * numeric range, null should be returned. If {@link MonetaryContext#getPrecision()} returns a value > 0 this
+     * method is required to provide a maximal amount.
+     *
      * @return the maximum possible number, or null.
      */
     NumberValue getMaxNumber();
 
     /**
      * Get the minimum possible number that this type can represent. If the numeric model has no limitations on the
-     * numeric range, null should be returned.
+     * numeric range, null should be returned.  If {@link MonetaryContext#getPrecision()} returns a value > 0 this
+     * method is required to provide a maximal amount.
+     *
      * @return the minimum possible number, or null.
      */
     NumberValue getMinNumber();
 
-	/**
-	 * Sets the {@link MonetaryContext} to be used.
-	 * 
-	 * @param monetaryContext
-	 *            the {@link MonetaryContext} to be used, not {@code null}.
-	 * @throws MonetaryException
-	 *             when the {@link MonetaryContext} given exceeds the capabilities supported by this
-	 *             factory type.
-	 * @return This factory instance, for chaining.
-	 * @see #getMaximalMonetaryContext()
-	 */
-	MonetaryAmountFactory<T> setContext(
-			MonetaryContext monetaryContext);
+    /**
+     * Sets the {@link MonetaryContext} to be used.
+     *
+     * @param monetaryContext the {@link MonetaryContext} to be used, not {@code null}.
+     * @return This factory instance, for chaining.
+     * @throws MonetaryException when the {@link MonetaryContext} given exceeds the capabilities supported by this
+     *                           factory type.
+     * @see #getMaximalMonetaryContext()
+     */
+    MonetaryAmountFactory<T> setContext(MonetaryContext monetaryContext);
 
-	/**
-	 * Uses an arbitrary {@link MonetaryAmount} to initialize this factory. Properties reused are:
-	 * <ul>
-	 * <li>CurrencyUnit,</li>
-	 * <li>Number value,</li>
-	 * <li>MonetaryContext.</li>
-	 * </ul>
-	 * 
-	 * @param amount
-	 *            the amount to be used, not {@code null}.
-	 * @throws MonetaryException
-	 *             when the {@link MonetaryContext} implied by {@code amount.getMonetaryContext()}
-	 *             exceeds the capabilities supported by this factory type.
-	 * @return this factory instance, for chaining.
-	 */
-	default MonetaryAmountFactory<T> setAmount(MonetaryAmount amount){
+    /**
+     * Uses an arbitrary {@link MonetaryAmount} to initialize this factory. Properties reused are:
+     * <ul>
+     * <li>CurrencyUnit,</li>
+     * <li>Number value,</li>
+     * <li>MonetaryContext.</li>
+     * </ul>
+     *
+     * @param amount the amount to be used, not {@code null}.
+     * @return this factory instance, for chaining.
+     * @throws MonetaryException when the {@link MonetaryContext} implied by {@code amount.getMonetaryContext()}
+     *                           exceeds the capabilities supported by this factory type.
+     */
+    default MonetaryAmountFactory<T> setAmount(MonetaryAmount amount){
         setCurrency(amount.getCurrency());
         setNumber(amount.getNumber());
         setContext(amount.getMonetaryContext());
         return this;
     }
 
-	/**
-	 * Creates a new instance of {@link MonetaryAmount}, using the current data set on this factory.
-	 * 
-	 * @return the corresponding {@link MonetaryAmount}.
-	 * @see #getAmountType()
-	 */
-	T create();
+    /**
+     * Creates a new instance of {@link MonetaryAmount}, using the current data set on this factory.
+     *
+     * @return the corresponding {@link MonetaryAmount}.
+     * @see #getAmountType()
+     */
+    T create();
 
-	/**
-	 * Returns the default {@link MonetaryContext} used, when no {@link MonetaryContext} is
-	 * provided.
-	 * <p>
-	 * The default context is not allowed to exceed the capabilities of the maximal
-	 * {@link MonetaryContext} supported.
-	 * 
-	 * @see #getMaximalMonetaryContext()
-	 * @return the default {@link MonetaryContext}, never {@code null}.
-	 */
-	MonetaryContext getDefaultMonetaryContext();
+    /**
+     * Returns the default {@link MonetaryContext} used, when no {@link MonetaryContext} is
+     * provided.
+     * <p>
+     * The default context is not allowed to exceed the capabilities of the maximal
+     * {@link MonetaryContext} supported.
+     *
+     * @return the default {@link MonetaryContext}, never {@code null}.
+     * @see #getMaximalMonetaryContext()
+     */
+    MonetaryContext getDefaultMonetaryContext();
 
-	/**
-	 * Returns the maximal {@link MonetaryContext} supported, for requests that exceed these maximal
-	 * capabilities, an {@link ArithmeticException} must be thrown.
-	 * 
-	 * @return the maximal {@link MonetaryContext} supported, never {@code null}
-	 */
-	default MonetaryContext getMaximalMonetaryContext(){
+    /**
+     * Returns the maximal {@link MonetaryContext} supported, for requests that exceed these maximal
+     * capabilities, an {@link ArithmeticException} must be thrown.
+     *
+     * @return the maximal {@link MonetaryContext} supported, never {@code null}
+     */
+    default MonetaryContext getMaximalMonetaryContext(){
         return getDefaultMonetaryContext();
     }
 

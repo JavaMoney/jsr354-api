8f4b60003c6f22340eb8e89020b3c5ff80ee3d1f
atsticks <atsticks@gmail.com>
2015-04-07 11:57:53 +0200

    Added fileheader. Removed author tag.

--- src/main/java/javax/money/convert/ExchangeRateProviderSupplier.java
+++ src/main/java/javax/money/convert/ExchangeRateProviderSupplier.java
@@ -1,10 +1,19 @@
+/*
+ * CREDIT SUISSE IS WILLING TO LICENSE THIS SPECIFICATION TO YOU ONLY UPON THE
+ * CONDITION THAT YOU ACCEPT ALL OF THE TERMS CONTAINED IN THIS AGREEMENT.
+ * PLEASE READ THE TERMS AND CONDITIONS OF THIS AGREEMENT CAREFULLY. BY
+ * DOWNLOADING THIS SPECIFICATION, YOU ACCEPT THE TERMS AND CONDITIONS OF THE
+ * AGREEMENT. IF YOU ARE NOT WILLING TO BE BOUND BY IT, SELECT THE "DECLINE"
+ * BUTTON AT THE BOTTOM OF THIS PAGE. Specification: JSR-354 Money and Currency
+ * API ("Specification") Copyright (c) 2012-2015, Credit SUISSE All rights
+ * reserved.
+ */
 package javax.money.convert;
 
 import java.util.function.Supplier;
 
 /**
  * The supplier of {@link ExchangeRateProvider} that defines an implementations.
- * @author otaviojava
  */
 public interface ExchangeRateProviderSupplier extends Supplier<String> {
 

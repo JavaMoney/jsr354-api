98423ba13ca2eee531d32920d869065228d8dc10
atsticks <atsticks@gmail.com>
2015-04-07 13:38:04 +0200

    Ensured API compatibility java7/8. Removed unused release plugin entry.

--- src/main/java/javax/money/convert/ExchangeRateProviderSupplier.java
+++ src/main/java/javax/money/convert/ExchangeRateProviderSupplier.java
@@ -1,24 +1,27 @@
 /*
- * CREDIT SUISSE IS WILLING TO LICENSE THIS SPECIFICATION TO YOU ONLY UPON THE
- * CONDITION THAT YOU ACCEPT ALL OF THE TERMS CONTAINED IN THIS AGREEMENT.
- * PLEASE READ THE TERMS AND CONDITIONS OF THIS AGREEMENT CAREFULLY. BY
- * DOWNLOADING THIS SPECIFICATION, YOU ACCEPT THE TERMS AND CONDITIONS OF THE
- * AGREEMENT. IF YOU ARE NOT WILLING TO BE BOUND BY IT, SELECT THE "DECLINE"
- * BUTTON AT THE BOTTOM OF THIS PAGE. Specification: JSR-354 Money and Currency
- * API ("Specification") Copyright (c) 2012-2015, Credit SUISSE All rights
- * reserved.
- */
+* CREDIT SUISSE IS WILLING TO LICENSE THIS SPECIFICATION TO YOU ONLY UPON THE
+* CONDITION THAT YOU ACCEPT ALL OF THE TERMS CONTAINED IN THIS AGREEMENT.
+* PLEASE READ THE TERMS AND CONDITIONS OF THIS AGREEMENT CAREFULLY. BY
+* DOWNLOADING THIS SPECIFICATION, YOU ACCEPT THE TERMS AND CONDITIONS OF THE
+* AGREEMENT. IF YOU ARE NOT WILLING TO BE BOUND BY IT, SELECT THE "DECLINE"
+* BUTTON AT THE BOTTOM OF THIS PAGE. Specification: JSR-354 Money and Currency
+* API ("Specification") Copyright (c) 2012-2015, Credit SUISSE All rights
+* reserved.
+*/
 package javax.money.convert;
 
-import java.util.function.Supplier;
 
 /**
- * The supplier of {@link ExchangeRateProvider} that defines an implementations.
- */
-public interface ExchangeRateProviderSupplier extends Supplier<String> {
+* A supplier of {@link ExchangeRateProvider} name that references an implementation.
+*/
+@FunctionalInterface
+public interface ExchangeRateProviderSupplier{
 
-	/**
-	 * @return description of the implementation of {@link ExchangeRateProvider}
-	 */
-	String getDescription();
+    /**
+     * Get the provider name. This signatire equals to the signrature of java.util.function.Supplier in Java 8.
+     *
+     * @return the provider name, not null.
+     */
+    String get();
+
 }

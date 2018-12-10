69131ea753ab5270c09289defa8ff3cd5964ed8c
Werner Keil <werner.keil@gmx.net>
2018-12-10 23:21:36 +0100

    56: Resolve License issue
    Task-Url: https://github.com/JavaMoney/jsr354-api/issues/issues/56

--- src/main/java/javax/money/format/AmountFormatQueryBuilder.java
+++ src/main/java/javax/money/format/AmountFormatQueryBuilder.java
@@ -1,10 +1,18 @@
 /*
- * CREDIT SUISSE IS WILLING TO LICENSE THIS SPECIFICATION TO YOU ONLY UPON THE CONDITION THAT YOU
- * ACCEPT ALL OF THE TERMS CONTAINED IN THIS AGREEMENT. PLEASE READ THE TERMS AND CONDITIONS OF THIS
- * AGREEMENT CAREFULLY. BY DOWNLOADING THIS SPECIFICATION, YOU ACCEPT THE TERMS AND CONDITIONS OF
- * THE AGREEMENT. IF YOU ARE NOT WILLING TO BE BOUND BY IT, SELECT THE "DECLINE" BUTTON AT THE
- * BOTTOM OF THIS PAGE. Specification: JSR-354 Money and Currency API ("Specification") Copyright
- * (c) 2012-2014, Credit Suisse All rights reserved.
+ * Copyright 2012-2016 Credit Suisse
+ * Copyright 2018 Werner Keil, Otavio Santana, Trivadis AG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package javax.money.format;
 

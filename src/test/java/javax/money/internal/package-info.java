eb58afc7ab553556bf85833707a0e202c25ca7db
Werner Keil <werner.keil@gmx.net>
2019-07-31 19:28:29 +0200

    # WARNING: head commit changed in the meantime
    56: Resolve License issue
    
    Task-Url: https://github.com/JavaMoney/jsr354-api/issues/issues/56

--- src/test/java/javax/money/internal/package-info.java
+++ src/test/java/javax/money/internal/package-info.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2012-2016 Credit Suisse
- * Copyright 2018 Werner Keil, Otavio Santana, Trivadis AG
+ * Copyright 2018-2019 Werner Keil, Otavio Santana, Trivadis AG
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
